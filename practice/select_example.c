ba85e75eb21ecadcc45282189bf5c3da3470c9f2
re1n-e <raghavendrabargali@gmail.com>
2025-05-27 20:54:10 +0530

    commit

--- practice/select_example.c
+++ practice/select_example.c
@@ -55,7 +55,7 @@ int main(int argc, char **argv)
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_len = sizeof(client_addr);
     fd_set read_fds, write_fds;
-
+    int nfds = 0, freeSlot = -1;
     init_clients();
 
     // Create a socket
@@ -72,9 +72,10 @@ int main(int argc, char **argv)
     server_addr.sin_port = htons(PORT);
 
     // Bind
-    if (bind(listen_fd, (sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+    if (bind(listen_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
     {
         perror("bind");
+        close(listen_fd);
         exit(EXIT_FAILURE);
     }
 
@@ -82,8 +83,84 @@ int main(int argc, char **argv)
     if (listen(listen_fd, 10) == -1)
     {
         perror("listen");
+        close(listen_fd);
         exit(EXIT_FAILURE);
     }
 
     printf("Server listening on port: %d\n", PORT);
+
+    while (1)
+    {
+        // reset FDS
+        FD_ZERO(&read_fds);
+        FD_ZERO(&write_fds);
+
+        // Add the listening socket to the read set
+        FD_SET(listen_fd, &read_fds);
+        nfds = listen_fd + 1;
+
+        // Add active connections to the read set
+        for (int i = 0; i < MAX_CLIENT; i++)
+        {
+            if (clientstates[i].fd != -1)
+            {
+                FD_SET(clientstates[i].fd, &read_fds);
+                if (clientstates[i].fd >= nfds)
+                    nfds = clientstates[i].fd + 1;
+            }
+        }
+
+        // wait for an activity on one of the fds
+        if (select(nfds, &read_fds, &write_fds, NULL, NULL) == -1)
+        {
+            perror("select");
+            close(listen_fd);
+            exit(EXIT_FAILURE);
+        }
+
+        // Check for new connetions
+        if (FD_ISSET(listen_fd, &read_fds))
+        {
+            if ((conn_fd = accept(listen_fd, (struct sockaddr *)&client_addr, &client_len)) == -1)
+            {
+                perror("accept");
+                close(listen_fd);
+                exit(EXIT_FAILURE);
+            }
+            printf("New connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+            // Find a free slot for a new connection
+            freeSlot = find_free_client();
+            if (freeSlot == -1)
+            {
+                printf("Server full: closing new connections\n");
+                close(conn_fd);
+            }
+            else
+            {
+                clientstates[freeSlot].fd = conn_fd;
+                clientstates[freeSlot].state = STATE_CONNECTED;
+            }
+        }
+
+        for (int i = 0; i < MAX_CLIENT; i++)
+        {
+            if (clientstates[i].fd != -1 && FD_ISSET(clientstates[i].fd, &read_fds))
+            {
+                ssize_t bytes_read = read(clientstates[i].fd, &clientstates[i].buffer, sizeof(clientstates[i].buffer));
+                if (bytes_read <= 0)
+                {
+                    close(clientstates[i].fd);
+                    clientstates[i].fd = -1;
+                    clientstates[i].state = STATE_DISCONNECTED;
+                    printf("Closing connection or error\n");
+                }
+                else
+                {
+                    printf("Recived Data from client: %s\n", clientstates[i].buffer);
+                }
+            }
+        }
+    }
+    return 0;
 }

16ab5e6b3171e302323aa574e41d9ad14e0414ff
re1n-e <raghavendrabargali@gmail.com>
2025-05-30 21:41:02 +0530

    commit

--- practice/poll_example.c
+++ practice/poll_example.c
@@ -6,6 +6,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/time.h>
+#include <poll.h>
 
 #define MAX_CLIENT 256
 #define PORT 8080
@@ -49,12 +50,24 @@ int find_free_client()
     return -1;
 }
 
+int find_client_by_fd(int fd)
+{
+    for (int i = 0; i < MAX_CLIENT; i++)
+    {
+        if (clientstates[i].fd == fd)
+            return i;
+    }
+    return -1;
+}
+
 int main(int argc, char **argv)
 {
     int listen_fd, conn_fd, slotFd;
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_len = sizeof(client_addr);
-    fd_set read_fds, write_fds;
+
+    struct pollfd fds[MAX_CLIENT + 1];
+    int opt = 1;
     int nfds = 0, freeSlot = -1;
     init_clients();
 
@@ -64,6 +77,17 @@ int main(int argc, char **argv)
         perror("Socket");
         exit(EXIT_FAILURE);
     }
+
+    if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(fds, 0, sizof(fds));
+    fds[0].fd = listen_fd;
+    fds[0].events = POLLIN;
+    nfds++;
 
     // set up server address structure
     memset(&server_addr, 0, sizeof(server_addr));
@@ -91,69 +115,70 @@ int main(int argc, char **argv)
 
     while (1)
     {
-        // reset FDS
-        FD_ZERO(&read_fds);
-        FD_ZERO(&write_fds);
-
-        // Add the listening socket to the read set
-        FD_SET(listen_fd, &read_fds);
-        nfds = listen_fd + 1;
-
-        // Add active connections to the read set
+        int ii = 1;
         for (int i = 0; i < MAX_CLIENT; i++)
         {
             if (clientstates[i].fd != -1)
             {
-                FD_SET(clientstates[i].fd, &read_fds);
-                if (clientstates[i].fd >= nfds)
-                    nfds = clientstates[i].fd + 1;
+                fds[ii].fd = clientstates[i].fd;
+                fds[ii++].events = POLLIN;
             }
         }
 
-        // wait for an activity on one of the fds
-        if (select(nfds, &read_fds, &write_fds, NULL, NULL) == -1)
+        // wait for event on one of the socket
+        int n_events = poll(fds, nfds, -1); // no timeout;
+        if (n_events == -1)
         {
-            perror("select");
-            close(listen_fd);
+            perror("POLL");
             exit(EXIT_FAILURE);
         }
 
-        // Check for new connetions
-        if (FD_ISSET(listen_fd, &read_fds))
+        if (fds[0].revents & POLLIN)
         {
-            if ((conn_fd = accept(listen_fd, (struct sockaddr *)&client_addr, &client_len)) == -1)
+            if ((conn_fd = accept(listen_fd, (struct sockaddr *)&client_addr, sizeof(client_addr))) == -1)
             {
                 perror("accept");
-                close(listen_fd);
-                exit(EXIT_FAILURE);
+                continue;
             }
-            printf("New connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+            printf("New connection from %s:%d", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
-            // Find a free slot for a new connection
             freeSlot = find_free_client();
             if (freeSlot == -1)
             {
-                printf("Server full: closing new connections\n");
+                printf("Server full. Closing Connections\n");
                 close(conn_fd);
             }
             else
             {
                 clientstates[freeSlot].fd = conn_fd;
                 clientstates[freeSlot].state = STATE_CONNECTED;
+                nfds++;
+                printf("Slot %d has fd %d\n", freeSlot, conn_fd);
             }
+            n_events--;
         }
 
-        for (int i = 0; i < MAX_CLIENT; i++)
+        for (int i = 1; i <= nfds && n_events > 0; i++)
         {
-            if (clientstates[i].fd != -1 && FD_ISSET(clientstates[i].fd, &read_fds))
+            if (fds[i].revents & POLLIN)
             {
+                n_events--;
+                int fd = fds[i].fd;
+                int slot = find_client_by_fd(fd);
                 ssize_t bytes_read = read(clientstates[i].fd, &clientstates[i].buffer, sizeof(clientstates[i].buffer));
                 if (bytes_read <= 0)
                 {
-                    close(clientstates[i].fd);
-                    clientstates[i].fd = -1;
-                    clientstates[i].state = STATE_DISCONNECTED;
-                    printf("Closing connection or error\n");
+                    close(fd);
+                    if (slot == -1)
+                    {
+                        printf("Tried to close fd that didn't exist\n");
+                    }
+                    else
+                    {
+                        clientstates[i].fd = -1;
+                        clientstates[i].state = STATE_DISCONNECTED;
+                        printf("Closing connection or error\n");
+                    }
                 }
                 else
                 {
